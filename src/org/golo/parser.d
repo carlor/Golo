85affe7de5648e31e4cd7fc558b361b28e4000c4
Nathan M. Swan <nathanmswan@gmail.com>
2012-01-28 11:52:13 -0900

    filled in stubs ready for lexing, parsing

---
+++ src/org/golo/parser.d
@@ -0,0 +1,16 @@
+
+module org.golo.parser;
+
+private {
+	import org.golo.lexer;
+	import org.golo.prefs;
+	import org.golo.source;
+}
+
+public class ModuleParser {
+	
+	public this(Lexer lex, Prefs p) {
+		
+	}
+	
+}
